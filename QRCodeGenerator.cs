bbf69346581e29510a5157cb88ca22fe337acfc8
Fernando Rocha <rochaf@microsoft.com>
2023-08-31 06:50:27 -0700

    fixign website and colors

--- QRCodeGenerator.cs
+++ QRCodeGenerator.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using QRCoder;
-using static QRCoder.PayloadGenerator;
 
 namespace Microsoft.Samples
 {
@@ -21,22 +20,22 @@ namespace Microsoft.Samples
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string reqData = req.Query["ReqData"];
+            string content = req.Query["content"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            reqData = reqData ?? data?.url;
-            if (string.IsNullOrEmpty(reqData))
+            content = content ?? data?.url;
+            if (string.IsNullOrEmpty(content))
             {
                 return new BadRequestResult();
             }
 
-            var payload = reqData.ToString();
+            var payload = content.ToString();
             using (var qrGenerator = new QRCoder.QRCodeGenerator())
             {
                 var qrCodeData = qrGenerator.CreateQrCode(payload, QRCoder.QRCodeGenerator.ECCLevel.Q);
-                var qrCode = new PngByteQRCode(qrCodeData);
-                var qrCodeAsPng = qrCode.GetGraphic(20);
+                var qrCode = new BitmapByteQRCode(qrCodeData);
+                var qrCodeAsPng = qrCode.GetGraphic(15, "#FFFFFF", "#EF4135");
                 return new FileContentResult(qrCodeAsPng, "image/png");
             }
         }
